f793ceff0f1152dfdd8fbc5091d772b80be2226f
galkinnikolay <galkin_nikolay@icloud.com>
2023-04-23 23:57:52 +0300

    .\Zadacha34\

---
+++ Zadacha34/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными 
+// трёхзначными числами (длина массива 5 элементов). 
+// Напишите программу, которая покажет количество чётных чисел 
+// в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+Console.WriteLine("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine()); 
+int[] array = new int[arraySize]; 
+
+for (int i = 0; i < array.Length; i++)
+{
+    int randomNumber = new Random().Next(99, 1000);
+    array[i] = randomNumber;
+    Console.Write(array[i] + " ");
+}
+
+int even = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        even = even + 1;
+    }
+}
+
+Console.WriteLine($"Четных чисел: {even}");
