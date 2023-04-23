5aa0db76bbd80f53709f6bca9b1c18ad37e675be
galkinnikolay <galkin_nikolay@icloud.com>
2023-04-24 00:32:47 +0300

    .\Zadacha38\

---
+++ Zadacha38/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 38: Задайте массив вещественных чисел от -100 до 100 
+// (длина массива 5 элементов). Найдите разницу между максимальным 
+// и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+double[] array = new double[5]; 
+
+for (int i = 0; i < array.Length; i++)
+{
+    int randomNumber = new Random().Next(-100, 100);
+    array[i] = randomNumber;
+    Console.Write(array[i] + " ");
+}
+
+double min = array[0];
+double max = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (max < array[i])
+    max = array[i];
+    if (min > array[i])
+    min = array[i];
+}
+
+Console.WriteLine();
+Console.WriteLine($"Маскимальное значение: {max}");
+Console.WriteLine();
+Console.WriteLine($"Минимальное значение: {min}");
+Console.WriteLine();
+Console.WriteLine($"Разница между максимальным и минимальным элементов массива: {max - min}");
